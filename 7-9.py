a8c1c6384d131f083d978db4b4742046fa425bd9
observer <ozzyundead@gmail.com>
2023-10-30 09:10:44 +0200

    delete

--- 7-9.py
+++
@@ -1,7 +0,0 @@
-sandwich_orders = ['cheeze_sandwich', 'pastrami_sandwich', 'meat_sandwich', 'pastrami_sandwich', 'fish_sandwich', 'pastrami_sandwich']
-print('we have not pastrami')
-
-while 'pastrami_sandwich' in sandwich_orders:
-    sandwich_orders.remove('pastrami_sandwich')
-
-print(sandwich_orders)
